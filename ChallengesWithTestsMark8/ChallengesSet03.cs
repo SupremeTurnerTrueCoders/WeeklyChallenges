ba141031d30b4b3eba67879ba371323748b480e1
SupremeTurnerTrueCoders <email@example.com>
2024-06-25 21:19:10 -0400

    Completed Weekly Challenge

--- ChallengesWithTestsMark8/ChallengesSet03.cs
+++ ChallengesWithTestsMark8/ChallengesSet03.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,17 +8,63 @@ namespace ChallengesWithTestsMark8
     {
         public bool ArrayContainsAFalse(bool[] vals)
         {
-            throw new NotImplementedException();
+            if (vals == null)
+            {
+                return false;
+            }
+
+
+            foreach (var item in vals)
+            {
+                if (item == false)
+                {
+                    return true;
+                }
+            }
+            return false;
+             //return vals != null && vals.Length != 0 && vals.Contains(false);
         }
 
         public bool IsSumOfOddsOdd(IEnumerable<int> numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            {
+                return false;
+            }
+            int sum = 0;
+            foreach (var number in numbers)
+            {
+                if (number % 2 != 0)
+                {
+                   sum += number;
+                }
+            }
+
+
+           // return numbers != null && numbers.Where(x => x % 2 != 0).Sum() % 2! = 0;
         }
 
         public bool PasswordContainsUpperLowerAndNumber(string password)
         {
-            throw new NotImplementedException();
+            bool isLetter = false;
+            bool isDigit = false;
+            bool isUpper = false;
+
+            foreach (var letter in password)
+            {
+                if (char.IsLower(letter))
+                {
+                    isLetter = true;
+                }
+                if (char.IsDigit(letter))
+                {
+                    isDigit = true;
+                }
+                if (char.IsUpper(letter))
+                {
+                    isUpper = true;
+                }
+            }
         }
 
         public char GetFirstLetterOfString(string val)
